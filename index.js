01549e91d6cf157d70e57087ff54834a9b79c9c7
AbhinawKumarDeonath <95974692+AbhinawKumarDeonath@users.noreply.github.com>
2022-09-27 11:28:33 +0530

    revele animation

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ menu.addEventListener('click', () =>
     navmanu.classList.toggle("active")
 });
 
+
+// SCROLL REVEAL
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:'2000',
+    reset:true
+})
+sr.reveal(".intro",{delay:200});
+sr.reveal(".image",{delay:300});
+sr.reveal(".project-a");
